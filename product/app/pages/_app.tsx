cf7dfd84cd0fe27e042a40a780c798b3b47feb3b
Rodion Chachura <geekrodion@gmail.com>
2024-12-03 07:58:51 +0400

    Simplify MyApp component: remove persistent theme state management and set default theme to dark

--- product/app/pages/_app.tsx
+++ product/app/pages/_app.tsx
@@ -2,13 +2,8 @@ import type { AppProps } from 'next/app'
 import { ReactNode } from 'react'
 import { GlobalStyle } from '@lib/ui/css/GlobalStyle'
 import { Inter } from 'next/font/google'
-import { ThemePreference } from '@lib/ui/theme/ThemePreference'
 import { DarkLightThemeProvider } from '@lib/ui/theme/DarkLightThemeProvider'
 import { Page } from '@lib/next-ui/Page'
-import {
-  usePersistentState,
-  PersistentStateKey,
-} from '../state/persistentState'
 import { WebsiteLayout } from '../layout/WebsiteLayout'
 
 const inter = Inter({
@@ -24,13 +19,8 @@ function MyApp({ Component, pageProps }: MyAppProps) {
   const getLayout = Component.getLayout || ((page: ReactNode) => page)
   const component = getLayout(<Component {...pageProps} />)
 
-  const [theme, setTheme] = usePersistentState<ThemePreference>(
-    PersistentStateKey.ThemePreference,
-    'dark',
-  )
-
   return (
-    <DarkLightThemeProvider value={theme} onChange={setTheme}>
+    <DarkLightThemeProvider value={'dark'}>
       <GlobalStyle fontFamily={inter.style.fontFamily} />
       <WebsiteLayout>{component}</WebsiteLayout>
     </DarkLightThemeProvider>
