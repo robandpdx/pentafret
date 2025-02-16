18b0ba589cc473a4511a343b46eb2995d471fd76
Rodion Chachura <geekrodion@gmail.com>
2025-02-16 10:58:03 +0400

    Add max-width constraint to ScaleManager VStack for improved layout

--- product/app/scale/manage/ScaleManager.tsx
+++ product/app/scale/manage/ScaleManager.tsx
@@ -5,7 +5,7 @@ import { ManageScale } from './ManageScale'
 export const ScaleManager = () => {
   return (
     <VStack alignItems="center">
-      <VStack gap={20} alignItems="start">
+      <VStack style={{ maxWidth: 480 }} gap={20} alignItems="start">
         <ManageRootNote />
         <ManageScale />
       </VStack>
