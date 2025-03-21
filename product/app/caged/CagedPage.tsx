51af29185e79b283b724a382aeca0f4b51abb305
Rodion Chachura <geekrodion@gmail.com>
2025-03-21 13:14:58 +0400

    Refactor CagedPage to integrate ManageCagedView and remove CagedManager
    - Replaced CagedManager with ManageCagedView in CagedPage for improved component structure.
    - Streamlined CagedPage layout by consolidating components and enhancing readability.
    - Removed the now redundant CagedManager component to simplify the codebase.

--- product/app/caged/CagedPage.tsx
+++ product/app/caged/CagedPage.tsx
@@ -4,31 +4,29 @@ import { CagedPageTitle } from './CagedPageTitle'
 import { cagedChords } from '@product/core/chords/caged'
 import { CagedProvider, CagedState } from './state/caged'
 import { ValueProp } from '@lib/ui/props'
-import { CagedManager } from './manage/CagedManager'
 import { CagedItem } from './CagedItem'
+import { ManageCagedView } from './manage/ManageCagedView'
 
-type CagedPageProps = ValueProp<CagedState>
-
-export const CagedPage = ({ value }: CagedPageProps) => {
-  return (
-    <CagedProvider value={value}>
-      <PageContainer>
-        <VStack gap={60}>
-          <CagedManager />
-          <CagedPageTitle />
-          <HStack
-            fullWidth
-            gap={60}
-            alignItems="center"
-            justifyContent="center"
-            wrap="wrap"
-          >
-            {cagedChords.map((chord) => (
-              <CagedItem key={chord} value={chord} />
-            ))}
-          </HStack>
+export const CagedPage = ({ value }: ValueProp<CagedState>) => (
+  <CagedProvider value={value}>
+    <PageContainer>
+      <VStack gap={60}>
+        <VStack alignItems="center">
+          <ManageCagedView />
         </VStack>
-      </PageContainer>
-    </CagedProvider>
-  )
-}
+        <CagedPageTitle />
+        <HStack
+          fullWidth
+          gap={60}
+          alignItems="center"
+          justifyContent="center"
+          wrap="wrap"
+        >
+          {cagedChords.map((chord) => (
+            <CagedItem key={chord} value={chord} />
+          ))}
+        </HStack>
+      </VStack>
+    </PageContainer>
+  </CagedProvider>
+)
